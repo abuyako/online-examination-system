14feae3da6f781fba74d8c0c72a27830f809f947
abuyako <abuyako@gmail.com>
2019-11-08 11:58:41 +0300

    create and design front-end for the admin

--- admin/login.php
+++ admin/login.php
@@ -7,12 +7,33 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="admin.css">
+    <title>login</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+     <div class="container jumbotron mt-3 text-center fix">
+        <h1 class="display-4 text-light"> Online Examination </h1>
+        <p class="text-light"> Admin Pannel</p>
+     </div>
 
+     <div class="container fix02">
+      <form action="" method="">
+        <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" name="username" placeholder="Enter username">
+        </div>
+        <div class="form-group">
+        <label for="Password">Password</label>
+        <input type="text" class="form-control" name="Password" placeholder="Enter password">
+        </div>
+        <button type="submit" class="btn">Submit</button>
+      </form>
+     </div>
+
+      <div class="container jumbotron mt-5 fix03">
+          <h2>Examination System</h2>
+      </div>
+   
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
