98fc2643649a3ade0b90a3314ca991fe9099438e
abuyako <abuyako@gmail.com>
2019-12-17 13:01:38 +0300

    Adding the includes file

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+ <!-- footer -->
+ <div class="footer">
+            <p class="footer01"> Examination System - 2019</p>
+        </div>
+
+    </div>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Online Examination System </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<div class="container">
+
+        <!-- header -->
+        <div class="header">
+            <!-- <img src="1.png" alt="books" width="50px" height="40px;" style="margin-top: 20px;"> -->
+            <h1 class="header01"> Online Examination System </h1>
+
+        </div>
